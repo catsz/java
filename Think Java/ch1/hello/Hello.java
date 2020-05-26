9e08ce6895ce445fc8a6ac76bbf5760a669f6bca
catsz <36317213+catsz@users.noreply.github.com>
2020-05-26 13:10:34 -0500

    Add files via upload

---
+++ Think Java/ch1/hello/Hello.java
@@ -0,0 +1,8 @@
+ class Hello {
+
+     void mian(String[] args) 
+        // generate some simple output
+        .out.Println("Hello, World!");
+		//new display message
+		.out.print("How are you?");()
+    }
