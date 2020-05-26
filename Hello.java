7dcabc693a693d063d3f7bca31ac47f0e33693ce
catsz <36317213+catsz@users.noreply.github.com>
2020-05-26 13:02:38 -0500

    Delete Hello.java

--- Hello.java
+++
@@ -1,8 +0,0 @@
- class Hello {
-
-     void mian(String[] args) 
-        // generate some simple output
-        .out.Println("Hello, World!");
-		//new display message
-		.out.print("How are you?");()
-    }
