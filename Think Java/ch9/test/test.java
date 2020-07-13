45c4de1e55836175407886aacc77afea79a1b8cd
catsz <griffinvmartin@gmail.com>
2020-07-13 13:53:47 -0500

    added ch9

---
+++ Think Java/ch9/test/test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class test {
+	public static Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		double test = 100;
+		int test2 = 200;
+		System.out.print("lasjkdf" + test);
+	}
+}
