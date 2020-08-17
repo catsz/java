6a7436db87c3078dff98804e648261a4b8490a3e
catsz <griffinvmartin@gmail.com>
2020-08-17 13:11:18 -0500

    compiled Date

--- Think Java/ch11/Date/Date.java
+++ Think Java/ch11/Date/Date.java
@@ -29,15 +29,15 @@ public class Date {
 		return this.day;
 	}
 	
-	public void setYear(year) {
+	public void setYear(int year) {
 		this.year = year;
 	}
 	
-	public void setMonth(month) {
+	public void setMonth(int month) {
 		this.month = month;
 	}
 	
-	public void setDay(day) {
+	public void setDay(int day) {
 		this.day = day;
 	}
 	
