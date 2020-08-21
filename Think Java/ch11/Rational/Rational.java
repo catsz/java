698ceb1c921bfd436e5d7cab0219ef97cb49a430
catsz <griffinvmartin@gmail.com>
2020-08-21 14:00:36 -0500

    added to rational

--- Think Java/ch11/Rational/Rational.java
+++ Think Java/ch11/Rational/Rational.java
@@ -9,8 +9,26 @@ public class Rational {
 		this.denominator = 1;
 	}
 	
+	public Rational(int numerator, int denominator) {
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
+	
 	public static void printRational(Rational ratio) {
-		System.out.printf("%d/%d\n", ratio.getNumerator(), ratio.getDenominator());
+		System.out.printf("%d/%d", ratio.getNumerator(), ratio.getDenominator());
+	}
+	
+	public double toDouble() {
+		double numerator = this.numerator;
+		double denominator = this.denominator;
+		return numerator / denominator;
+	}
+	
+	private static int findGCD(int number1, int number2) {
+		if (number2 == 0) {
+			return number1;
+		}
+		return findGCD(number2, number1%number2);
 	}
 	
 	public int getNumerator() {
@@ -29,11 +47,57 @@ public class Rational {
 		this.numerator = Numerator;
 	}
 	
-	public static void main(String[] args) {
-		Rational ratio = new Rational();
-		ratio.setNumerator(5);
-		ratio.setDenominator(10);
+	public String toString() {
+		return String.format("%d/%d", this.numerator, this.denominator);
+	}
+	
+	public void negate() {
+		if (numerator < 0) {
+			this.numerator = Math.abs(this.numerator);
+		} else {
+			this.numerator = 0 - this.numerator;
+		}
+		if (denominator < 0) {
+			this.denominator = Math.abs(this.denominator);
+		} else {
+			this.denominator = 0 - this.denominator;
+		}
+	}
+	
+	public void invert() {
+		int setDenominator = this.numerator;
 		
-		Rational.printRational(ratio);
+		this.numerator = this.denominator;
+		this.denominator = setDenominator;
+	}
+	
+	public boolean equals(Rational ratio) {
+		if (this.toDouble() == ratio.toDouble()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public Rational reduce() {
+		int GCD = findGCD(this.numerator, this.denominator);
+		
+		return new Rational(this.numerator / GCD, this.denominator / GCD);
+	}
+	
+	public Rational add(Rational ratio) {
+		Rational r1 = new Rational(this.numerator * ratio.getDenominator(), this.denominator * ratio.getDenominator());
+		Rational r2 = new Rational(ratio.getNumerator() * this.denominator, this.denominator * ratio.getDenominator());
+		Rational rtn = new Rational(r1.getNumerator() + r2.getNumerator(), this.denominator * ratio.getDenominator());
+		
+		
+		return rtn.reduce();
+	}
+	
+	public static void main(String[] args) {
+		Rational r1 = new Rational(5, 10);
+		Rational r2 = new Rational(8, 20);
+		
+		System.out.print(r1.add(r2));
 	}
 }
