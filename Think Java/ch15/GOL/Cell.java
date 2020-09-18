6544a50c7479bb8d3dd914ecd9fc7ff243447de9
catsz <griffinvmartin@gmail.com>
2020-09-18 14:50:25 -0500

    added ch15 with game of life

---
+++ Think Java/ch15/GOL/Cell.java
@@ -0,0 +1,70 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * A square at a fixed location that changes color.
+ */
+public class Cell {
+
+    public static final Color[] COLORS = {Color.WHITE, Color.BLACK};
+
+    private final int x;
+    private final int y;
+    private final int size;
+    private int state;
+
+    /**
+     * Constructs a new cell, initially turned off.
+     * 
+     * @param x the X coordinate
+     * @param y the Y coordinate
+     * @param size number of pixels
+     */
+    public Cell(int x, int y, int size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.state = 0;
+    }
+
+    /**
+     * Draws the cell on the screen.
+     * 
+     * @param g graphics context
+     */
+    public void draw(Graphics g) {
+        g.setColor(COLORS[state]);
+        g.fillRect(x + 1, y + 1, size - 1, size - 1);
+        g.setColor(Color.LIGHT_GRAY);
+        g.drawRect(x, y, size, size);
+    }
+
+    /**
+     * @return true if the cell is OFF
+     */
+    public boolean isOff() {
+        return state == 0;
+    }
+
+    /**
+     * @return true if the cell is ON
+     */
+    public boolean isOn() {
+        return state == 1;
+    }
+
+    /**
+     * Sets the cell's state to OFF.
+     */
+    public void turnOff() {
+        state = 0;
+    }
+
+    /**
+     * Sets the cell's state to ON.
+     */
+    public void turnOn() {
+        state = 1;
+    }
+
+}
